75dcf7d441ca7f8225ac79d9a2f6744278c145ce
Graham Barber <grahammbarber@gmail.com>
2018-08-01 13:34:34 -0700

    feat(server): add logging and body parsing

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import e from 'express';
+import morgan from 'morgan';
 import signale from 'signale';
+import bodyParser from 'body-parser';
 
 import { generate } from './routes/generate';
 
@@ -7,6 +9,9 @@ signale.pending('Starting server...');
 
 const app = e();
 const port = process.env.PORT as string || 3000 as number;
+
+app.use(bodyParser.json());
+app.use(morgan(process.env.NODE_ENV === 'production' ? 'short' : 'dev'));
 
 app.post('/generate', generate);
 
