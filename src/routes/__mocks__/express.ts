78374b4639c3c3c6a7fae8e904486960ed65cacd
Graham Barber <grahammbarber@gmail.com>
2018-08-03 14:10:31 -0700

    test: remove type mocks

--- src/routes/__mocks__/express.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, Send, NextFunction } from 'express';
-
-export interface MockResponse extends Response {
-  send: MockSend;
-}
-
-export interface MockSend extends Send {
-  mock: jest.MockContext<MockSend>;
-}
