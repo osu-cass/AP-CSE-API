06f80f7ee015926357ac32fd2baf29de2f7c81d4
Dan Van Horn <dan.vanhorn17@gmail.com>
2018-08-31 15:33:43 -0700

    test(server): updates to test for dbInit route creation

--- src/server/server.spec.ts
+++ src/server/server.spec.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { use, listen, get, post} from '../../__mocks__/express';
 import passport from 'passport';
-import { home, greet } from '../routes';
+import { home, greet, dbInit } from '../routes';
 import { Server } from './';
 
 jest.mock('passport');
@@ -18,13 +18,14 @@ describe('Server', () => {
     });
 
     it('initializes and configures correctly', () => {
-        expect.assertions(6);
+        expect.assertions(7);
         expect(routesSpy).toHaveBeenCalledTimes(1);
         expect(configureSpy).toHaveBeenCalledTimes(1);
         expect(passportUseSpy).toHaveBeenCalledTimes(1);
         expect(use).toHaveBeenCalledTimes(3);
         expect(get).toHaveBeenCalledWith('/', home);
-        expect(post).toHaveBeenCalledWith('/greet', server.authenticate, greet);
+        expect(post.mock.calls[0]).toEqual(['/greet', server.authenticate, greet]);
+        expect(post.mock.calls[1]).toEqual(['/init', dbInit]);
     });
 
     it('returns an http.Server instance', () => {
