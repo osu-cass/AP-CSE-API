125c6d9b0036e8658404f3eda6bfe985ad5150ab
Dan Van Horn <dan.vanhorn17@gmail.com>
2018-09-04 12:52:38 -0700

    test(server): remove authentication

--- src/server/server.spec.ts
+++ src/server/server.spec.ts
@@ -1,5 +1,5 @@
 import e from 'express';
-import { use, listen, get, post} from '../../__mocks__/express';
+import { use, listen, get, post} from '../__mocks__/express';
 import passport from 'passport';
 import { home, greet, dbInit } from '../routes';
 import { Server } from './';
@@ -10,21 +10,21 @@ describe('Server', () => {
     let server: Server;
     const routesSpy: jest.SpyInstance = jest.spyOn(Server.prototype, 'routes');
     const configureSpy: jest.SpyInstance = jest.spyOn(Server.prototype, 'configure');
-    const authenticateSpy: jest.SpyInstance = jest.spyOn(passport, 'authenticate');
-    const passportUseSpy: jest.SpyInstance = jest.spyOn(passport, 'use');
+    // const authenticateSpy: jest.SpyInstance = jest.spyOn(passport, 'authenticate');
+    // const passportUseSpy: jest.SpyInstance = jest.spyOn(passport, 'use');
 
     beforeAll(() => {
         server = new Server();
     });
 
     it('initializes and configures correctly', () => {
-        expect.assertions(7);
+        expect.assertions(6);
         expect(routesSpy).toHaveBeenCalledTimes(1);
         expect(configureSpy).toHaveBeenCalledTimes(1);
-        expect(passportUseSpy).toHaveBeenCalledTimes(1);
-        expect(use).toHaveBeenCalledTimes(3);
+        // expect(passportUseSpy).toHaveBeenCalledTimes(1);
+        expect(use).toHaveBeenCalledTimes(2);
         expect(get).toHaveBeenCalledWith('/', home);
-        expect(post.mock.calls[0]).toEqual(['/greet', server.authenticate, greet]);
+        expect(post.mock.calls[0]).toEqual(['/greet', greet]);
         expect(post.mock.calls[1]).toEqual(['/init', dbInit]);
     });
 
@@ -35,9 +35,9 @@ describe('Server', () => {
         expect(listen).toHaveBeenCalledTimes(1);
     });
 
-    it('authenticates the user', () => {
-        expect.assertions(1);
-        server.authenticate();
-        expect(authenticateSpy).toHaveBeenCalledWith('local', { failureRedirect: '/', session: false });
-    });
+    // it('authenticates the user', () => {
+    //     expect.assertions(1);
+    //     server.authenticate();
+    //     expect(authenticateSpy).toHaveBeenCalledWith('local', { failureRedirect: '/', session: false });
+    // });
 });
