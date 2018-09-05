20d33103a2261644767cbabc9edf0fbcdf2416ae
Dan Van Horn <dan.vanhorn17@gmail.com>
2018-09-05 11:04:12 -0700

    feat(data model): adds data model for claim

---
+++ src/models/claim/index.ts
@@ -0,0 +1,12 @@
+import { ITarget } from "../target";
+
+export interface IClaim {
+    title: string;
+    claimNumber: string;
+    grades: string[] | number;
+    subject: string;
+    description?: string;
+    shortCode:string;
+    domain?: string;
+    target: ITarget[];
+};
