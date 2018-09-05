0d97051daf4a39d013495e8956c4408e331c0c2e
Dan Van Horn <dan.vanhorn17@gmail.com>
2018-09-05 11:03:43 -0700

    feat(data model): add data model for target

---
+++ src/models/target/index.ts
@@ -0,0 +1,28 @@
+export interface ITarget {
+    title: string;
+    shortCode: string;
+    description: string;
+    standards: string[];
+    stdDesc: string[];
+    DOK: string[];
+    DOKDesc: string[];
+    shortDOK: string[];
+    type: string;
+    clarification: string;
+    heading: string;
+    evidence: string[];
+    vocab: string;
+    tools: string;
+    stimInfo: string;
+    devNotes: string;
+    complexity: string;
+    dualText: string;
+    accessibility: string;
+    stem: string[];
+    taskDescription: string[];
+    taskModel: string[];
+    examples: string[];
+    rubrics: string[];
+    stimulus: string[];
+    shortStem: string[];
+}
