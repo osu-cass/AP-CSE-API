93a2945d1e0c676396d5d91fed1308ba3fd9c2c9
Dan Van Horn <dan.vanhorn17@gmail.com>
2018-09-18 11:25:05 -0700

    chore(search): updates import statement in root index file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Server } from './server';
 import http from 'http';
-import { SearchClient } from './search';
+import { SearchClient } from './dal/search';
 
 const server: Server = new Server();
 
