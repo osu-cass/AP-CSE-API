7d9185e389d96ce826893f4fbe19e64b7258849a
Dan Van Horn <dan.vanhorn17@gmail.com>
2018-09-19 14:42:14 -0700

    feat(search): adds search client class

--- src/dal/search/index.ts
+++ src/dal/search/index.ts
@@ -1,4 +1,5 @@
-import { Client } from 'elasticsearch';
+import { Client, SearchResponse } from 'elasticsearch';
+import { IClaim } from '../../models/claim';
 
 /**
  * Wrapper class for the elastic search client
@@ -13,8 +14,67 @@ export class SearchClient {
         });
     }
 
-    // public docExists() {
+    private async exists(id: string): Promise<boolean> {
+        let exists: boolean;
+        try {
+            // tslint:disable-next-line:no-unsafe-any
+            exists = await this.client.exists({
+                id,
+                index: `cse`,
+                type: `claim`,
+            });
+        } catch (err) {
+            throw err;
+        }
 
-    // }
+        return exists;
+    }
+
+    // tslint:disable-next-line:no-reserved-keywords
+    private async delete(id: string) {
+        try {
+            await this.client.delete({
+                id,
+                index: `cse`,
+                type: `claim`,
+            });
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    public async insertDocuments(claims: IClaim[]): Promise<void> {
+        let id = 0;
+        for(const claim of claims) {
+            delete claim._id;
+            try {
+                if(await this.exists(`${id}`)) {
+                    await this.delete(`${id}`);
+                }
+                await this.client.create({
+                    id: `${id}`,
+                    index: `cse`,
+                    type: `claim`,
+                    body: claim
+                });
+            } catch (err) {
+                throw err;
+            }
+            id++;
+        }
+    }
+
+    public async search(q: string) {
+        let result: SearchResponse<{}>;
+        try {
+            result = await this.client.search({
+                q,
+                index: `cse`,
+                type: `claim`,
+            });
+        } catch (err) {
+            throw err;
+        }
+    }
 }
 
