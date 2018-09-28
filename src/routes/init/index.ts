d1800792ee2d5e0c18a19e9ad8020f31f336d2c3
Horne <hornee@oregonstate.edu>
2018-09-28 11:41:16 -0700

    refactor(init): uses request context variables

--- src/routes/init/index.ts
+++ src/routes/init/index.ts
@@ -4,25 +4,20 @@ import { importDbEntries } from './db/index';
 import { applyTracing } from '../../utils/tracer/index';
 import { CSEResponse } from '../../server/index';
 import { IClaim } from '../../models/claim/index';
-import { IDbClient, DbClient } from '../../dal/interface/index';
 
 export const handler = async (req: Request, res: CSEResponse): Promise<void> => {
+    const { dbClient } = res.locals;
+    let result: InsertWriteOpResult | undefined;
     try {
         const output: IClaim[] = await importDbEntries();
-        const dbArgs: IDbClient = {
-            url: 'mongodb://mongo',
-            port: 27017,
-            dbName: 'cse'
-        };
-        const client: DbClient = new DbClient(dbArgs);
-        await client.connect();
+        await dbClient.connect();
         // tslint:disable-next-line
-        const result: InsertWriteOpResult | undefined = await client.insert(output);
-        res.header('Content-Type', 'application/json');
-        res.send(result ? result.result : 'Nope');
+        result = await dbClient.insert(output);
     } catch (err) {
         throw err;
     }
+    res.header('Content-Type', 'application/json');
+    res.send(result ? result.result : 'Nope');
 };
 
 export const dbInit = applyTracing('/init', handler);
--- src/routes/init/init.spec.ts
+++ src/routes/init/init.spec.ts
@@ -1,56 +1,48 @@
 import { handler as dbInit } from './index';
-import { Request } from 'express';
-import { CSEResponse } from '../../server/index';
+import { Request, Response } from 'express';
 
 jest.mock('express');
 jest.mock('./db/index', () => ({
-    importDbEntries: jest.fn().mockResolvedValueOnce('{}').mockResolvedValueOnce('{}').mockImplementationOnce(() => {
-        const err = new Error('I am Error.');
-        throw err;
-    })
+    importDbEntries: jest
+    .fn()
+    .mockResolvedValueOnce('{}')
+    .mockResolvedValueOnce('{}')
+    .mockRejectedValueOnce(new Error('I am Error.'))
 }));
-
-jest.mock('../../dal/interface', () => {
-    return {
-        DbClient: jest.fn().mockImplementationOnce(() => ({
-            connect: jest.fn().mockResolvedValue({}),
-            insert: jest.fn().mockResolvedValue({ result: 'good' })
-        })).mockImplementationOnce(() => ({
-            connect: jest.fn().mockResolvedValue({}),
-            insert: jest.fn().mockResolvedValue(undefined)
-        })).mockImplementationOnce(() => ({
-            connect: jest.fn().mockResolvedValue({}),
-            insert: jest.fn().mockResolvedValue(undefined)
-        }))
-    };
-});
 
 describe('init', () => {
     let req: Partial<Request>;
-    let res: Partial<CSEResponse>;
-
+    let res: Partial<Response>;
+    let dbClient;
     beforeAll(() => {
+        dbClient = {
+            connect: jest.fn().mockResolvedValue({}),
+            insert: jest.fn().mockResolvedValueOnce({ result: 'good' }).mockResolvedValue(undefined)
+        };
         req = {};
         res = {
             header: jest.fn(),
             send: jest.fn(),
+            locals: {
+                dbClient
+            }
         };
     });
 
     it('initializes the database with data', async () => {
         expect.assertions(1);
-        await dbInit(<Request>req, <CSEResponse>res);
+        await dbInit(<Request>req, <Response>res);
         expect(res.send).toBeCalledWith('good');
     });
 
     it('fails to init database', async () => {
         expect.assertions(1);
-        await dbInit(<Request>req, <CSEResponse>res);
+        await dbInit(<Request>req, <Response>res);
         expect(res.send).toBeCalledWith('Nope');
     });
     it('throws an error', async () => {
         try {
-            await dbInit(<Request>req, <CSEResponse>res);
+            await dbInit(<Request>req, <Response>res);
         }
         catch (err) {
             expect(err).toEqual(new Error('I am Error.'));
