ede64bf4a4636d86dc141d098f498a408fd67881
Dan Van Horn <dan.vanhorn17@gmail.com>
2018-10-15 15:58:52 -0700

    test(healthcheck): fixed healthcheck test

--- src/routes/health/health.spec.ts
+++ src/routes/health/health.spec.ts
@@ -2,20 +2,27 @@ import { handler as healthCheck, Health, setRouteHealth } from '.';
 import { Request, Response } from 'express';
 
 describe('API Routing Health Check', () => {
+  let req: Partial<Request> ;
+  let res: Partial<Response>;
+  let status: Health;
+
+  beforeAll(() => {
+    res = {
+      send: jest.fn(),
+      status: jest.fn()
+    };
+    req = {};
+  });
+
   it('sets route health', () => {
-    const status: Health = Health.busy;
-    const req: Partial<Request> = {};
+    status = Health.busy;
     req.path = '/init';
     setRouteHealth(status, <Request>req);
   });
+
   it('returns a response', () => {
-    const req: Partial<Request> = {};
-    const res: Partial<Response> = {
-      send: jest.fn()
-    };
-
     healthCheck(<Request>req, <Response>res);
-
     expect(res.send).toBeCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(200);
   });
 });
