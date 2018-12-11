d96e4a2716c5a70d00ec6ac29ceeb4e78e45f464
Horne <hornee@oregonstate.edu>
2018-12-11 13:13:15 -0800

    fix(import-script): removes performance tasks from import

--- src/dal/import/index.ts
+++ src/dal/import/index.ts
@@ -359,10 +359,10 @@ export function getTarget(claim: IClaim, jsonData: ISpecDocument, DOKDOC: ISpecD
         }
         const splitStatement = fullStatement.split(' ');
         if (p.CFItemType === 'Evidence Required' && splitStatement.length > 2) {
-            target.evidence.push({
-              evTitle: abbreviatedStatement,
-              evDesc: fullStatement
-            });
+          target.evidence.push({
+            evTitle: abbreviatedStatement,
+            evDesc: fullStatement
+          });
         }
         // This block handles a bug in the CASE API where some CFItems are missing their CFItemType property
         if (
@@ -488,7 +488,7 @@ function getClaimDesc(
 export function consolidate(claimArray: IClaim[]): IClaim[] {
   let tempArray = [];
   let claimHolder;
-  const finalArray: IClaim[] = [];
+  let finalArray: IClaim[] = [];
   const myArray = [];
   let unique: string[] = [];
   for (const claims of claimArray) {
@@ -525,6 +525,7 @@ export function consolidate(claimArray: IClaim[]): IClaim[] {
     }
   }
   handlePT(finalArray);
+  finalArray = removePT(finalArray);
 
   return finalArray;
 }
@@ -534,7 +535,7 @@ export function handlePT(finalArray: IClaim[]) {
   let tempIdx;
 
   PTArr = finalArray.filter(claim => claim.title.includes('Performance'));
-    PTArr.forEach((PT) => {
+  PTArr.forEach(PT => {
     for (const targ of PT.target) {
       if (parseInt(PT.grades[0], 10) < 8) {
         tempIdx = finalArray.findIndex(claim => {
@@ -544,5 +545,8 @@ export function handlePT(finalArray: IClaim[]) {
       }
     }
   });
+}
 
+export function removePT(finalArray: IClaim[]) {
+  return finalArray.filter(claim => !claim.title.includes('Performance'));
 }
