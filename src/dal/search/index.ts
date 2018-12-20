0ad473b3bbac80bc41580750e8fdd93f4cfc2385
Dan Van Horn <dan.vanhorn17@gmail.com>
2018-12-20 15:31:32 -0800

    fix(search): update size of results for grades query to 100

--- src/dal/search/index.ts
+++ src/dal/search/index.ts
@@ -61,9 +61,10 @@ export class SearchClient implements ISearchClient {
     }
 
     if (grades) {
-      body.query('bool', {
-        should: grades.split(',').map((grade: string) => ({ match: { grades: grade } }))
-      });
+      body.size(100).query('bool', {
+          should: grades.split(',').map((grade: string) => ({ match: { grades: grade } }))
+        }
+      );
     }
 
     if (claimNumber) {
