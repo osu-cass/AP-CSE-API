9204172f3398b8cfe826805ad492020d6e851183
Thomas Noelcke <noelcket@oregonstate.edu>
2019-01-30 09:52:32 -0800

    fix(image proxy): fix logic issue in hanlder

--- src/routes/image/index.ts
+++ src/routes/image/index.ts
@@ -10,11 +10,11 @@ import { IImageResponse, IImageParams } from '../../models/image/index';
 export const handler = async (req: Request, res: CSEResponse): Promise<void> => {
   const { url }: IImageParams = <IImageParams>req.query;
   let imageString: string | undefined;
+  const imageResponse: IImageResponse = {image: undefined, error: undefined};
   if(!url) {
     res.status(400);
   } else {
     imageString = await image2base64(url);
-    const imageResponse: IImageResponse = {image: undefined, error: undefined};
     if(!imageString) {
       imageResponse.error = 'Could not find image';
       res.status(404);
@@ -24,8 +24,9 @@ export const handler = async (req: Request, res: CSEResponse): Promise<void> => 
     }
 
     res.contentType('application/json');
-    res.send(imageResponse);
   }
+
+  res.send();
 };
 
 export const image = applyTracing('/image', handler);
